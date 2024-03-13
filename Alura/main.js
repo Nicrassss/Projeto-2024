dc216c7dd5f753720b010fab5bce6e163343e707
Nicrassss <126926924+Nicrassss@users.noreply.github.com>
2024-03-13 20:06:22 -0300

    Add files via upload

--- Alura/main.js
+++ Alura/main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba.conteudo")
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -7,5 +8,20 @@ for(let i=0;i <botoes.length;i++){
             botoes[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
     }
+}
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1)
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = tempoFinal/1000;
+let minutos = Math.floor (segundos/60)
+return segundos;
 }
