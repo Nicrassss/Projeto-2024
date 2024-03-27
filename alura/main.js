b06711bf43e2f818cae552763f5f06d5881358a7
Nicrassss <126926924+Nicrassss@users.noreply.github.com>
2024-03-26 21:47:10 -0300

    hhhhhh

--- alura/main.js
+++ alura/main.js
@@ -15,9 +15,14 @@ for(let i=0;i <botoes.length;i++){
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1)
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo1 - tempoAtual;
@@ -28,6 +33,21 @@ let dias = Math.floor(horas / 24);
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
 
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+    function atualizaCronometro(){
+        for (let i=0; i<contadores.length;i++){
+            contadores[i].textContent = calculaTempo(tempos[i]);   
+        }
+    }
+    function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro(), 1000);
+    }
+    comecaCronometro();
